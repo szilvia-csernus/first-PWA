43b49a06ac5640bfb4932c2da257b23d1ef8b7bd
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-07-10 16:31:04 +0100

    add manifest and serviceworkers

---
+++ public/src/js/app.js
@@ -0,0 +1,17 @@
+let deferredPrompt;
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }).catch(function(err) {
+        console.log('ServiceWorker registration failed: ', err);
+    });
+}
+
+window.addEventListener('beforeinstallprompt', function(event) {
+    console.log('beforeinstallprompt fired');
+    event.preventDefault();
+    deferredPrompt = event;
+    showInstallPromotion();
+    return false;
+});
--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -4,6 +4,23 @@ var closeCreatePostModalButton = document.querySelector('#close-create-post-moda
 
 function openCreatePostModal() {
   createPostArea.style.display = 'block';
+  // Check if browser supports deferred prompt
+  if (deferredPrompt) {
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then(function(choiceResult) {
+      console.log(choiceResult.outcome);
+      // If the user accepted the prompt, log the result
+      if (choiceResult.outcome === 'dismissed') {
+        console.log('User cancelled installation');
+      } else {
+        console.log('User added to home screen');
+      }
+    });
+    // Set the deferred prompt to null
+    deferredPrompt = null;
+  };
 }
 
 function closeCreatePostModal() {
